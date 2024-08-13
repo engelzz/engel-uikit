581e738d1db63255207fa3f5e129b9cacd528ad2
Matheus Engel <102177203+engelzz@users.noreply.github.com>
2024-08-13 18:54:47 -0300

    feat: add card component

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import './styles/index.css';
 
 export * from './components/ui/Avatar';
 export * from './components/ui/Button';
+export * from './components/ui/Card';
 export * from './components/ui/DropdownMenu';
 export * from './components/ui/Input';
 
